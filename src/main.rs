195aac81a9032a5ded97e56420a036f3d293e4c0
Kevin Channon <kevinchannon@gmail.com>
2023-01-22 07:32:46 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod stateful;
 
 const FOO: stateful::State = stateful::State{id: 0, name: "foo"};
+const BAR: stateful::Event = stateful::Event{id: 0, name: "bar"};
 
 fn main() {
     
     println!("Hello, state {}", FOO.name);
 
-    let sm = stateful::StateMachine{states: [&FOO], actions: []};
-    println!("State machine with {} states and {} actions", sm.states.len(), sm.actions.len());
+    let sm = stateful::StateMachine{states: [&FOO], events: [&BAR], transitions: []};
+    println!("State machine with {} states and {} actions", sm.states.len(), sm.transitions.len());
 }
--- src/stateful/mod.rs
+++ src/stateful/mod.rs
@@ -3,11 +3,26 @@ pub struct State {
     pub name: &'static str,
 }
 
-pub type Action = fn(&'static State) -> &'static State;
+pub struct Event {
+    pub id: u32,
+    pub name: &'static str,
+}
 
-pub struct StateMachine<const STATE_COUNT: usize, const ACTION_COUNT: usize> {
+pub type Action = fn();
+
+pub struct Transition{
+    pub start: &'static State,
+    pub event: &'static Event,
+    pub action: Action,
+    pub end: &'static State
+}
+
+pub struct StateMachine<const STATE_COUNT: usize, const EVENT_COUNT: usize, const TRANSITION_COUNT: usize> {
     pub states: [&'static State; STATE_COUNT],
-    pub actions: [Action; ACTION_COUNT]
+    pub events: [&'static Event; EVENT_COUNT],
+    pub transitions: [Transition; TRANSITION_COUNT],
+
+    pub state: &'static State
 }
 
 #[cfg(test)]
@@ -15,32 +30,65 @@ mod test {
     use super::*;
 
     #[test]
-    fn state_name_and_id() {
+    fn state_has_name_and_id() {
         let s = State{ id: 0, name: "IDLE"};
         assert_eq!(0, s.id);
         assert_eq!("IDLE", s.name);
     }
 
     #[test]
+    fn event_has_name_and_id() {
+        let e = Event{ id: 0, name: "button press"};
+        assert_eq!(0, e.id);
+        assert_eq!("button press", e.name);
+    }
+
+    #[test]
     fn create_state_machine(){
-        fn test_action(start: &'static State) -> &'static State {
-            start
-        }
+        fn test_action() {}
 
         const IDLE: State = State{ id: 0, name: "IDLE"};
         const ACTIVE: State = State{ id: 1, name: "ACTIVE"};
 
+        const ACTIVATE: Event = Event{id: 0, name: "activate"};
+        const DEACTIVATE: Event = Event{id: 1, name: "deactivate"};
+
         let sm = StateMachine{
             states: [&IDLE, &ACTIVE],
-            actions: [test_action]
+            events: [&ACTIVATE, &DEACTIVATE],
+            transitions: [Transition{start: &IDLE, event: &ACTIVATE, action: test_action, end: &ACTIVE}]
         };
 
         assert_eq!(2, sm.states.len());
-        assert_eq!(1, sm.actions.len());
+        assert_eq!(2, sm.events.len());
+        assert_eq!(1, sm.transitions.len());
     }
 
     #[test]
-    fn run_until_runs_until_the_target_state_is_reached(){
+    fn reaches_the_correct_state_when_an_event_is_received() {
+        fn test_action() {}
 
+        const IDLE: State = State{ id: 0, name: "IDLE"};
+        const ACTIVE: State = State{ id: 1, name: "ACTIVE"};
+
+        const ACTIVATE: Event = Event{id: 0, name: "activate"};
+        const DEACTIVATE: Event = Event{id: 1, name: "deactivate"};
+        
+        let sm = StateMachine{
+            states: [
+                &IDLE, 
+                &ACTIVE
+                ],
+            events: [
+                &ACTIVATE,
+                &DEACTIVATE
+                ],
+            transitions: [
+                Transition{start: &IDLE, event: &ACTIVATE, action: test_action, end: &ACTIVE},
+                Transition{start: &ACTIVE, event: &DEACTIVATE, action: test_action, end: &IDLE}
+                ]
+        };
+
+        assert_eq!(IDLE, sm.state);
     }
 }
